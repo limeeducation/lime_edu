a549997940c04d5f852eac6c63f3c9743c49794b
shawwwnie <sanghoon1549@gmail.com>
2022-09-05 21:49:54 +0900

    Apply Footer

--- application/views/camp/camp.php
+++ application/views/camp/camp.php
@@ -1,5 +1,11 @@
-
-This is Camp Main Page
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    This is Camp Main Page
+</body>
 
 <?php
 include($_SERVER['DOCUMENT_ROOT']."/phil/application/views/layout/footer.php");
