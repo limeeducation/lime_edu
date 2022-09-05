ac14ae1b41c54524306c005baca4906696187372
shawwwnie <sanghoon1549@gmail.com>
2022-09-05 21:19:15 +0900

    Edit page connection

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -19,7 +19,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('camp/camp');
+		$this->load->view('welcome_message');
 	}
 	public function goCampPage(){
 	    $this->load->view('camp/camp');
