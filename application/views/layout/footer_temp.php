c033d5653b7b4e925b183483b38661a7e2328876
shawwwnie <sanghoon1549@gmail.com>
2022-09-07 22:17:44 +0900

    edit footer

--- application/views/layout/footer_temp.php
+++ application/views/layout/footer_temp.php
@@ -39,9 +39,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <a href="" class="text-white me-4">
           <i class="fab fa-linkedin"></i>
         </a>
-        <a href="" class="text-white me-4">
-          <i class="fab fa-github"></i>
-        </a>
       </div>
       <!-- Right -->
     </section>
@@ -69,15 +66,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <!-- Grid column -->
           <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
             <!-- Links -->
-            <h6 class="text-uppercase fw-bold">Contact</h6>
+            <h6 class="text-uppercase fw-bold">캐나다 본사</h6>
             <hr
                 class="mb-4 mt-0 d-inline-block mx-auto"
                 style="width: 60px; background-color: #7c4dff; height: 2px"
                 />
-            <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-            <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-            <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-            <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+            <p><i class="fas fa-home mr-3"></i> 55 Eglinton Ave East, Suite 601, Toronto, ON Canada M4P 1G8</p>
+            <p><i class="fas fa-envelope mr-3"></i> tglee@mylimeedu.com </p>
+            <p><i class="fas fa-phone mr-3"></i> + 1 647 427 2006</p>
           </div>
           <!-- Grid column -->
         </div>
