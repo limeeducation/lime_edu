5bdd97cfeec13375101776b41ad89b37c4c1add1
shawwwnie <sanghoon1549@gmail.com>
2022-09-13 23:43:51 +0900

    add application form

--- application/views/layout/footer_temp.php
+++ application/views/layout/footer_temp.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
 
