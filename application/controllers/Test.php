cbaaf8601f5cafcce2e6e761162454180e06c532
shawwwnie <sanghoon1549@gmail.com>
2022-09-14 23:32:49 +0900

    auth test

---
+++ application/controllers/Test.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/userguide3/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('test/test');
+	}
+	public function check_mobile(){
+		$sSiteCode		= "BZ073";			// 사이트 코드 (NICE평가정보에서 발급한 사이트코드)
+		$sSitePw		= "gZ1gsPiXjZLZ";			// 사이트 패스워드 (NICE평가정보에서 발급한 사이트패스워드)
+
+		$sJumin 		= "0000000000000";						// 주민번호 체크 안함
+		$sHP 			= $this->input->post('hp_num1').$this->input->post('hp_num2').$this->input->post('hp_num3'); // 휴대폰번호
+		$sRequestSeq 	= "SEQ00001";								// 요청SEQ_식별값
+		$sHPComp  		= "1";										// 이통사 구분안함
+
+
+		$scheckplus_path = $_SERVER['DOCUMENT_ROOT']."/SCheckPlus";
+
+		//인자값 : AUTH 사이트코드 사이트패스워드 주민등록번호 이통사구분(1/2/3) 휴대전화번호 요청SEQ(option)
+		$sResultData = `$scheckplus_path AUTH $sSiteCode $sSitePw $sJumin "" $sHPComp $sHP $sRequestSeq`;
+
+		//결과 : 응답코드|요청SEQ|응답SEQ
+		echo "결과 : ".$sResultData;
+	}
+}
