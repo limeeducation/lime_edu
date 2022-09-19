71f08a6a8acb92d668dd44d09390dd662e6a6d1f
shawwwnie <sanghoon1549@gmail.com>
2022-09-19 22:57:28 +0900

    add old main

---
+++ application/views/layout/old/head_ca.php
@@ -0,0 +1,137 @@
+<?
+include_once($_SERVER['DOCUMENT_ROOT']."/config/common.php");
+
+?>
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">  
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+  <meta property="og:image" content="./img/og_main.jpg" />
+
+  <title>라임에듀</title>
+  <link href="/css/style.css" rel="stylesheet" >
+  <link href="/css/sub_ca.css" rel="stylesheet" >
+  <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+  
+
+
+
+</head>
+
+
+<body>
+<div id="wrap">
+
+<div id="headerin">
+<header>
+	<h1 class="logo"><a href="/"><img src="/img/logo_lime.png" title="미국 캐나다 영국 전문가 라임에듀" alt="미국 캐나다 영국 전문가 라임에듀"></a></h1>
+
+	<div class="top_call">
+		<div class="his"><a href=""><i class="far fa-building"></i><span>회사소개</span></a></div>
+		<div class="call"><a href=""><i class="fas fa-phone"></i><span>대표전화 02-2135-7699</span></a></div>
+	</div>
+
+	<div class="openSide"><a href="#"></a></div>
+
+	 <div class="side">
+		<div class="closeSide"><p><i class="fas fa-times"></i>닫기</p></div>  
+        <nav class="gnb ">
+            <ul>
+                <li class="mn_c">
+                    <a href="#" class="<?if($me=='1'){echo 'on';}?> class=" cate">캐나다 관리형유학</a>
+                    <div class="sub">
+                        <ul>
+                            <li class="side_dmn"><a href="/mng/bc.php" class=""><span>BC주 관리형 유학</span></a></li>
+                            <li class="side_dmn"><a href="/mng/ab.php" class=""><span>AB주 관리형 유학</span></a></li>
+                          <!--   <li class="side_dmn"><a href="/mng/on.php" class=""><span>ON주 관리형 유학</span></a></li> -->
+                        </ul>
+                    </div>
+                </li>
+                <li class="mn_c">
+                    <a href="#" class="<?if($me=='2'){echo 'on';}?> cate">국제 사립학교</a>
+                    <div class="sub">
+                        <ul>
+                            <li class="side_dmn"><a href="/intl/bicc.php" class=""><span>BICC</span></a></li>
+                            <li class="side_dmn"><a href="/intl/tale.php" class=""><span>TAIE</span></a></li>
+                            <li class="side_dmn"><a href="/intl/umc.php" class=""><span>UMC High School</span></a></li>
+                           <!-- <li class="side_dmn"><a href="/intl/sgb.php" class=""><span>SGA</span></a></li>-->
+                        </ul>
+                    </div>
+                </li>
+				
+				<li class="mn_c">
+                    <a href="#" class="<?if($me=='3'){echo 'on';}?> cate">명문보딩스쿨</a>
+                    <div class="sub">
+                        <ul>
+                            <li class="side_dmn"><a href="/bod/sch1.php" class=""><span>Lakefield College School</span></a></li>
+                            <li class="side_dmn"><a href="/bod/sch2.php" class=""><span>Ridely College</span></a></li>
+                            <li class="side_dmn"><a href="/bod/sch3.php" class=""><span>Pickering College</span></a></li>
+                            <!--<li class="side_dmn"><a href="/bod/sch4.php" class=""><span>St. Johns Academy</span></a></li>-->
+                            <li class="side_dmn"><a href="/bod/sch5.php" class=""><span>Bodwell High School</span></a></li>
+                        </ul>
+                    </div>
+                </li>
+
+				
+                <li class="mn_c">
+                    <a href="#" class="<?if($me=='4'){echo 'on';}?> cate">아이 혼자 공립학교</a>
+                    <div class="sub">
+                        <ul>
+                            <li class="side_dmn"><a href="/publ/on.php" class=""><span>요크가톨릭 교육청</span></a></li>
+                            <li class="side_dmn"><a href="/publ/on2.php" class=""><span>요크 교육청</span></a></li>
+							<li class="side_dmn"><a href="/publ/on3.php" class=""><span>토론토 가톨릭 교육청</span></a></li>
+							<li class="side_dmn"><a href="/publ/on4.php" class=""><span>토론토 교육청</span></a></li>
+							<li class="side_dmn"><a href="/publ/on5.php" class=""><span>노스밴쿠버 교육청</span></a></li>
+                            <li class="side_dmn"><a href="/publ/on6.php" class=""><span>랭리 교육청</span></a></li>
+							<li class="side_dmn"><a href="/publ/on7.php" class=""><span>밴쿠버 교육청</span></a></li>
+							<li class="side_dmn"><a href="/publ/on8.php" class=""><span>버나비 교육청</span></a></li>
+							<li class="side_dmn"><a href="/publ/on9.php" class=""><span>코퀴틀람 교육청</span></a></li>
+							<!--<li class="side_dmn"><a href="/publ/bc.php" class=""><span>코퀴틀람 교육청</span></a></li>-->
+                        </ul>
+                    </div>
+                </li>
+
+				
+                <li class="mn_c">
+                    <a href="/gdn/gdn.php" class="<?if($me=='5'){echo 'on';}?>">프리미엄 가디언 서비스</a>
+                </li>
+				
+                <li class="mn_c">
+                    <a href="/fm/family.php" class="<?if($me=='6'){echo 'on';}?> ">캐나다 명문대 편입</a>
+
+                </li>
+				
+   
+				
+                <li class="mn_c">
+                    <a href="#" class="<?if($me=='7'){echo 'on';}?> cate">컬리지</a>
+					<div class="sub">
+                        <ul>
+                            <li class="side_dmn"><a href="/col/major.php" class=""><span>전공별 컬리지</span></a></li>
+                            <li class="side_dmn"><a href="/col/area.php" class=""><span>지역별 컬리리</span></a></li>
+                        </ul>
+					</div>
+                </li>
+				
+                <li class="mn_c">
+                    <a href="#" class="<?if($me=='8'){echo 'on';}?> cate">교육제도 및 비자</a>
+                    <div class="sub">
+                        <ul>
+                            <li class="side_dmn"><a href="/visa/edu.php" class=""><span>캐나다 교육제도</span></a></li>
+                            <li class="side_dmn"><a href="/visa/ca_visa.php" class=""><span>캐나다 비자</span></a></li>
+                        </ul>
+                    </div>
+                </li>
+				
+
+            </ul>
+		</nav>
+	 </div><!--side-->
+
+</header>
+</div><!--headerin-->
+
---
+++ application/views/main/old_main.php
@@ -0,0 +1,476 @@
+<?php
+include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/old/head_ca.php');
+include_once($_SERVER['DOCUMENT_ROOT']."/inc/blog_list.php");
+?>
+
+
+<link href="/static/css/old/main.css" rel="stylesheet" >
+
+
+<div class="main_top">  
+		<div id="slide_wrap">
+		<ul style="display: none;">
+			<li data-img="/static/img/old/main/ca_main_slider01.jpg">
+				<div class="main_tx_all">
+					<div class="main_tx_box">
+						<div class="main_text">
+						<div class="mainT">
+							캐나다 유학 컨설팅 전문 기관으로<br>많은 고객들과 함께 하였습니다. 
+						</div>
+							<ul>
+								<li><p>누적 수속 학생수</p><span>210,00명</span></li>
+								<li><p>누적 방문고객</p><span>39,052명</span></li>
+								<li><p>해외교육기관제휴 </p><span>120개</span></li>
+							</ul>
+						</div>
+					</div>
+
+
+					<div class="blackbox">
+						<div class="bl_vtit">
+							<div class="bl_vtit1">전세계 <b>대학 입학 현황</b> </div>
+							<p class="bl_vtit3">최근 5년간 컨설턴트 기준</p>
+						</div>
+						<ul>
+							<li><div>김*희님</div><p>Fanshawe College 입학을 축하드립니다</p><span>21년 9월 학기</span> </li>
+							<li><div>박*윤님</div><p>Kingston University, London 입학을 축하드립니다</p><span>21년 9월 학기</span> </li>
+							<li><div>윤*이님</div><p>Fanshawe College 입학을 축하드립니다</p><span>21년 9월 학기</span> </li>
+							<li><div>김*수님</div><p>Fanshawe College 입학을 축하드립니다</p><span>21년 9월 학기</span> </li>
+							<li><div>김*혜님</div><p>Fanshawe College 입학을 축하드립니다</p><span>21년 9월 학기</span> </li>
+							<li><div>윤*정님</div><p>Fanshawe College 입학을 축하드립니다</p><span>21년 9월 학기</span> </li>
+							<li><div>김*님</div><p>Fanshawe College 입학을 축하드립니다</p><span>21년 9월 학기</span> </li>
+							<li><div>김*혜님</div><p>Fanshawe College 입학을 축하드립니다</p><span>21년 9월 학기</span> </li>
+							<li><div>김*희님</div><p>Fanshawe College 입학을 축하드립니다</p><span>21년 9월 학기</span> </li>
+						</ul>
+					</div>
+				</div>
+			</li>
+			<li data-img="/static/img/old/main/ca_main_slider02.jpg">
+					<div class="mainText">
+						<div>
+							<span>26년간의 캐나다 유학 컨설팅</span>
+							<span>많은 고객과 해외 교육기관이 </span>
+							<span>함께 했습니다</span>
+						</div>
+					</div>
+			</li>
+		</ul>
+	</div><!--slide_wrap-->
+</div>
+
+
+
+<div id="main">
+
+<section class="main_con1">
+	<div class="mainc1 bx1">
+		<div class="mc1box bx1">
+
+			<div class="swiper c1Swiper">
+			  <div class="swiper-wrapper">
+
+				<div class="swiper-slide">
+					<ul class="mc1_im_ul">
+					<?
+					for($i=0;$row1 = mysql_fetch_array($result);$i++){
+						$img_file = explode("|",$row1['filename_real1']);
+						
+					?>
+						<li><a href="/board/blog_view.php?idx=<?=$row1['idx']?>">
+							<div class="tx">
+								<p class="tit"><?=$row1['title']?></p>
+								<div><?=strip_tags($row1['contents'])?></div>
+							</div>
+							<div class="im"><img src="/static/img/old/data/board/<?=$img_file[0]?>"></div></a>
+						</li>	
+						<?}?>
+					</ul>		
+				</div><!--swiper-slide-->
+			  
+				<div class="swiper-slide">
+					<ul class="mc1_im_ul">
+					<?
+					for($i=0;$row2 = mysql_fetch_array($result2);$i++){
+						$img_file = explode("|",$row2['filename_real1']);
+					?>
+						<li><a href="/board/blog_view.php?idx=<?=$row2['idx']?>">
+							<div class="tx">
+								<p class="tit"><?=$row2['title']?></p>
+								<div><?=strip_tags($row2['contents'])?></div>
+								<!--<span>
+									조회 <b>103</b> 
+									댓글 <b>8</b>
+								</span>-->
+							</div>
+							<div class="im"><img src="/static/img/old/data/board/<?=$img_file[0]?>"></div></a>
+						</li>
+						<?}?>
+					</ul>		
+				</div><!--swiper-slide-->
+			  </div>
+			  <div class="swiper-pagination"></div>
+			</div>
+
+
+		</div>
+		<div class="mc1box bx2">
+			<h2 class="main_tit">전공별 대학</h2>
+			<ul class="mc1_ic_ul scrollbar">
+				<li><a href="./col/major.php"><i class="fas fa-desktop"></i><p>Computer Science & Engineering</p></a></li>
+				<li><a href="./col/major.php"><i class="fas fa-wrench"></i><p>Mechanical Engineering</p></a></li>
+				<li><a href="./col/major.php"><i class="fas fa-stethoscope"></i><p>Health & Medical Science</p></a></li>
+				<li><a href="./col/major.php"><i class="fas fa-briefcase"></i><p>Business & Commerce</p></a></li>
+				<li><a href="./col/major.php"><i class="fas fa-truck-pickup"></i><p>Architectural Engineering</p></a></li>
+				<li><a href="./col/major.php"><i class="fas fa-palette"></i><p>Art & Design</p></a></li>
+				<li><a href="./col/major.php"><i class="fas fa-share-alt"></i><p>Social Scinece</p></a></li>
+				<li><a href="./col/major.php"><i class="fas fa-vials"></i><p>Biomedical Enginerring</p></a></li>
+				<li><a href="./col/major.php"><i class="far fa-newspaper"></i><p>Journlism</p></a></li>
+				<li><a href="./col/major.php"><i class="fas fa-superscript"></i><p>Mathmatics</p></a></li>
+				<li><a href="./col/major.php"><i class="fas fa-space-shuttle"></i><p>Aerospace Engineering</p></a></li>
+				<li><a href="./col/major.php"><i class="fas fa-user-astronaut"></i><p>Pilot Training</p></a></li>
+				<li><a href="./col/major.php"><i class="far fa-chart-bar"></i><p>Statistics</p></a></li>
+				<li><a href="./col/major.php"><i class="fas fa-book-medical"></i><p>Psychology &Neuroscience</p></a></li>
+				<li><a href="./col/major.php"><i class="fas fa-school"></i><p>Education</p></a></li>
+				<li><a href="./col/major.php"><i class="fas fa-user-nurse"></i><p>Nursing</p></a></li>
+			</ul>
+		</div>
+	</div><!--mainc1-->
+</section>
+
+<section class="main_con2">
+	<h2 class="main_tit">라임에듀 추천 프로그램</h2>
+	<div class="main_sl_box">
+		<div class="swiper c2Swiper">
+		  <div class="swiper-wrapper">
+			<div class="swiper-slide"><a href="/bod/sch2.php"><div><img src="/static/img/old/prog_ca01_new.jpg" alt=""></div></a><a href="/fm/family.php"><div><img src="/static/img/old/prog_ca05_new.jpg" alt=""></div></a></div>
+			<div class="swiper-slide"><a href="/mng/bc.php"><div><img src="/static/img/old/prog_ca02_new.jpg" alt=""></div></a><div><a href="/intl/bicc.php"><img src="/static/img/old/prog_ca06_new.jpg" alt=""></a></div></div>
+			<div class="swiper-slide"><div><a href="/gdn/gdn.php"><img src="/static/img/old/prog_ca03_new.jpg" alt=""></a></div><div><a href="/col/major.php"><img src="/static/img/old/prog_ca07_new.jpg" alt=""></a></div></div>
+			<div class="swiper-slide"><div><a href="/publ/on.php"><img src="/static/img/old/prog_ca04_new.jpg" alt=""></a></div><div><a href="/visa/edu.php"><img src="/static/img/old/prog_ca08_new.jpg" alt=""></a></div></div>
+		  </div>
+		   <div class="swiper-pagination"></div>
+		</div>
+	</div>
+</section>
+
+<section class="main_con3">
+	<h2 class="main_tit">학생들이 말하는 후기</h2>
+    
+	<div class="swiper c3Swiper">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide"><img src="/static/img/old/after_ka_1.jpg"></div>
+        <div class="swiper-slide"><img src="/static/img/old/after_ka_2.jpg"></div>
+        <div class="swiper-slide"><img src="/static/img/old/after_ka_3.jpg"></div>
+        <div class="swiper-slide"><img src="/static/img/old/after_ka_4.jpg"></div>
+        <div class="swiper-slide"><img src="/static/img/old/after_ka_5.jpg"></div>
+		 <div class="swiper-slide"><img src="/static/img/old/after_ka_7.jpg"></div>
+      </div>
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-pagination"></div>
+    </div>
+</section>
+
+<section class="main_con4"><h2 class="main_tit">캐나다 지역별 추천 대학</h2>
+	<div class="main_map ca">
+		<div class="map_img"></div>
+		<div class="map_sch_list">
+			<div class="tab on">
+				<h3 class="tit1">British</h3>
+				<div class="sch_list br">
+					<p class="ntit">British Columbia</p>
+					<ul class="scrollbar">
+						<li>Alexander College</li>
+						<li>British Columbia Institute of Technology</li>
+						<li>Camosun College</li>
+						<li>Canadian College</li>
+						<li>Capilano University</li>
+						<li>Coast Mountain College</li>
+						<li>College of New Caledonia</li>
+						<li>College of the Rockies</li>
+						<li>Columbia College</li>
+						<li>Create Career College</li>
+						<li>Douglas College</li>
+						<li>Educacentre College (French language institution)</li>
+						<li>Emily Carr University of Art and Design</li>
+						<li>Eton College, Vancouver</li>
+						<li>First College</li>
+						<li>Kwantlen Polytechnic University</li>
+						<li>Langara College</li>
+						<li>North Island College</li>
+						<li>Northern Lights College</li>
+						<li>Okanagan College</li>
+						<li>Royal Roads University</li>
+						<li>Royal Roads University</li>
+						<li>Selkirk College</li>
+						<li>Simon Fraser University</li>
+						<li>Sprott Shaw College</li>
+						<li>Thompson Rivers University</li>
+						<li>University of British Columbia</li>
+						<li>University of Northern British Columbia</li>
+						<li>University of the Fraser Valley</li>
+						<li>University of Victoria</li>
+						<li>Vancouver Community College</li>
+						<li>Vancouver Institute of Media Arts (VanArts)</li>
+						<li>Vancouver Island University</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="tab">
+				<h3 class="tit2">Alberta</h3>
+				<div class="sch_list ab">
+					<p class="ntit">Alberta</p>
+					<ul class="scrollbar">
+						<li>Ambrose University	</li>
+						<li>Bow Valley College	</li>
+						<li>Burman University	</li>
+						<li>Concordia University of Edmonton	</li>
+						<li>Grande Prairie Regional College	</li>
+						<li>Keyano College	</li>
+						<li>Lakeland College	</li>
+						<li>MacEwan University	</li>
+						<li>Medicine Hat College	</li>
+						<li>Mount Royal University	</li>
+						<li>NorQuest College	</li>
+						<li>Northern Alberta Institute of Technology	</li>
+						<li>Northern Lakes College	</li>
+						<li>Red Deer College	</li>
+						<li>SAIT Polytechnic	Calgary</li>
+						<li>St Mary's University	</li>
+						<li>The King’s University	</li>
+						<li>University of Alberta	</li>
+						<li>University of Calgary</li>	
+						<li>University of Lethbridge</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="tab">
+				<h3 class="tit3">Ontario</h3>
+				<div class="sch_list on">
+					<p class="ntit">Ontario</p>
+					<ul class="scrollbar">
+						<li>Algoma University</li>
+						<li>Algonquin College</li>
+						<li>Brock University</li>
+						<li>Cambrian College</li>
+						<li>Canadore College</li>
+						<li>Carleton University</li>
+						<li>Centennial College</li>
+						<li>Collège Boréal</li>
+						<li>Conestoga College</li>
+						<li>Confederation College</li>
+						<li>Durham College</li>
+						<li>Fanshawe College</li>
+						<li>Fleming College</li>
+						<li>George Brown College</li>
+						<li>Georgian College</li>
+						<li>Humber College</li>
+						<li>Lakehead University</li>
+						<li>Lambton College</li>
+						<li>Laurentian University </li>
+						<li>Loyalist College</li>
+						<li>McMaster University</li>
+						<li>Mohawk College</li>
+						<li>Niagara College</li>
+						<li>Nipissing University</li>
+						<li>Northern College</li>
+						<li>Ontario College of Art and Design University</li>
+						<li>Ontario Tech University</li>
+						<li>Queen's University at Kingston</li>
+						<li>Royal Military College</li>
+						<li>Royal Military College of Canada </li>
+						<li>Ryerson University</li>
+						<li>Sault College</li>
+						<li>Seneca College</li>
+						<li>Sheridan College</li>
+						<li>Springfield College Brampton</li>
+						<li>St. Clair College</li>
+						<li>St. Lawrence College</li>
+						<li>Trent University</li>
+						<li>University of Guelph</li>
+						<li>University of Ottawa </li>
+						<li>University of Toronto</li>
+						<li>University of Waterloo</li>
+						<li>University of Western Ontario</li>
+						<li>University of Windsor</li>
+						<li>Wilfrid Laurier University</li>
+						<li>York University</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="tab">
+				<h3 class="tit4">Quebec</h3>
+				<div class="sch_list qc">
+					<p class="ntit">Quebec</p>
+					<ul class="scrollbar">
+						<li>Bishop's University</li>
+						<li>Concordia University</li>
+						<li>McGill University</li>
+						<li>Université de Montréal</li>
+						<li>Université de Sherbrooke</li>
+						<li>Université Laval</li>
+					</ul>
+				</div>
+			</div>
+
+		</div><!---->
+	</div>
+</section>
+</div><!--main-->
+
+<?php
+include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/footer_temp.php');
+?>
+
+<script>
+	$(".map_sch_list .tab h3").click(function(){
+		$(".map_sch_list .tab").removeClass("on");
+		$(this).parent(".tab").addClass("on");
+	});
+
+</script>
+<script>//게시글
+  var swiper = new Swiper(".c1Swiper", {  
+    loop: true,
+	pagination: {
+	  el: ".swiper-pagination",
+	  dynamicBullets: true,
+	},
+  });
+//추천프로그램
+
+
+
+  var swiper = new Swiper(".c2Swiper", {
+	  loop:'true',
+	  autoplay : {  
+		  delay : 4000,   
+		  disableOnInteraction : false, 
+		},
+	  slidesPerView: 2,
+	  spaceBetween: 10, 
+	  pagination: {
+		  el: ".swiper-pagination",
+		  clickable: true,
+		},
+		//반응형 조건 속성
+		  breakpoints: { 
+			  640: { 
+				slidesPerView: 3, 
+			  },
+			  1020: {
+				loop:false,
+			   slidesPerView: 4,
+			   spaceBetween: 25, 
+				   touchRatio: 0 
+			  },
+		  },
+
+});
+
+
+
+//후기
+  var swiper = new Swiper(".c3Swiper", {
+
+        slidesPerView: 2,
+        spaceBetween: 20,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+
+		  breakpoints: { 
+			  1020: {
+			   slidesPerView: 4,
+			   spaceBetween: 30, 
+				   touchRatio: 0 
+			  },
+		  },
+		   observer: true,
+
+      });
+
+</script>
+
+<script src="/js/lc-micro-slider.js" type="text/javascript"></script> 
+    <script type="text/javascript">
+    const timing = 8000;
+        
+        
+    new lc_micro_slider('#slide_wrap', {
+        fixed_slide_type    : 'image',  
+        slide_fx            : 'fade',
+        animation_time	    : 2000,
+        slideshow_time	    : timing,
+        nav_arrows          : true,  
+        slideshow_cmd       : true,
+        autoplay            : true,
+        pause_on_hover      : true,
+        addit_classes       : ['lcms_default_theme']    
+    });
+        
+        
+        
+    //// ken burns effect
+    let ken_burns_intval,
+        curr_comb = '';
+        
+    document.getElementById('slide_wrap').addEventListener('lcms_slide_shown', (e) => {
+        const $subj = e.target,
+              slide_index = e.detail.slide_index;
+
+        if(ken_burns_intval) {
+            clearInterval(ken_burns_intval);    
+        }
+        ken_burns_intval = setInterval(() => {
+            ken_burns_fx($subj, slide_index);
+        }, timing);    
+
+        ken_burns_fx($subj, slide_index);
+    });
+        
+    const ken_burns_fx = function($subj, slide_index) {
+              vert_opts     = ["top", "bottom"],
+              horiz_opts    = ["left", "right"];
+        
+        if(document.getElementById('lcms_kenburns')) {
+            document.getElementById('lcms_kenburns').remove();        
+        }
+
+        const vert_rule  = vert_opts[Math.floor(Math.random() * vert_opts.length)], 
+              horiz_rule = horiz_opts[Math.floor(Math.random() * horiz_opts.length)];
+              
+        // force movement
+        if(curr_comb == vert_rule+horiz_rule) {
+            ken_burns_fx($subj, slide_index);      
+        }
+        
+        
+        let animation = {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0
+        };
+        animation[ vert_rule ] = '-20%';
+        animation[ horiz_rule ] = '-20%';
+
+        document.querySelector('#slide_wrap .lcms_slide[data-index="'+ slide_index +'"] .lcms_bg').animate(
+            animation,
+            {
+                duration: timing,
+                iterations: 1,
+                fill: 'forwards'
+            }
+        );       
+    };  
+    </script>
