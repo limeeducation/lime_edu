4974ef9e1c6b4a989193602274fbbde34e566562
shawwwnie <sanghoon1549@gmail.com>
2022-09-19 23:36:42 +0900

    edit old main

--- application/views/main/old_main.php
+++ application/views/main/old_main.php
@@ -72,7 +72,7 @@ include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/old/head_ca.php');
 				<div class="swiper-slide">
 					<ul class="mc1_im_ul">
 					<?
-					for($i=0;$row1 = mysql_fetch_array($result);$i++){
+					for($i=0;$row1 = mysqli_fetch_array($result);$i++){
 						$img_file = explode("|",$row1['filename_real1']);
 						
 					?>
@@ -90,7 +90,7 @@ include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/old/head_ca.php');
 				<div class="swiper-slide">
 					<ul class="mc1_im_ul">
 					<?
-					for($i=0;$row2 = mysql_fetch_array($result2);$i++){
+					for($i=0;$row2 = mysqli_fetch_array($result2);$i++){
 						$img_file = explode("|",$row2['filename_real1']);
 					?>
 						<li><a href="/board/blog_view.php?idx=<?=$row2['idx']?>">
