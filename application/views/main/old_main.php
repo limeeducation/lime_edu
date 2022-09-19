874cde8bcb73aed9359ce107caa75c8db54acd18
shawwwnie <sanghoon1549@gmail.com>
2022-09-19 23:41:10 +0900

    edit old main

--- application/views/main/old_main.php
+++ application/views/main/old_main.php
@@ -5,9 +5,20 @@ include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/old/head_ca.php');
 
 
 <link href="/static/css/old/main.css" rel="stylesheet" >
-
+<?= echo "<br>";?>
+<?= echo "<br>";?>
+<?= echo "<br>";?>
+<?= echo "result = ";?>
+<?= echo "<br>";?>
 <?php print_r($result); ?>
+<?= echo "<br>";?>
+<?= echo "<br>";?>
+<?= echo "<br>";?>
+<?= echo "result2 = ";?>
 <?php print_r($result2); ?>
+<?= echo "<br>";?>
+<?= echo "<br>";?>
+<?= echo "<br>";?>
 <div class="main_top">  
 		<div id="slide_wrap">
 		<ul style="display: none;">
