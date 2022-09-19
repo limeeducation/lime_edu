528b367bd9e89020ba628b9c3ddf6fecdcac890b
shawwwnie <sanghoon1549@gmail.com>
2022-09-19 23:27:44 +0900

    edit old main

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -2,9 +2,21 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
-
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->load->model(array('old_model'));
+	}
 	public function index()
 	{
-		$this->load->view('main/old_main');
+		$query = "select * from ms_board_table where isdel != '1' order by idx desc limit 0,3";
+		$result = $this->old_model->run_query();
+		$query = "select * from ms_board_table where isdel != '1' order by idx desc limit 3,6";
+		$result2 = $this->old_model->run_query();
+		$this->document->view('main/old_main', array(
+        			'result' 	=> $result,
+        			'result2'	=> $result2
+        ));
 	}
 }
