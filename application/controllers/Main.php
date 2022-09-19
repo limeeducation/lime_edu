a93e766305df281fc83caaa8f2b15730ccb7a858
shawwwnie <sanghoon1549@gmail.com>
2022-09-19 23:34:33 +0900

    edit old main

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -7,7 +7,6 @@ class Main extends CI_Controller {
 		parent::__construct();
 		$this->load->database();
 		$this->load->model(array('old_model'));
-		$this->load->library( 'document' );
 	}
 	public function index()
 	{
@@ -15,7 +14,7 @@ class Main extends CI_Controller {
 		$result = $this->old_model->run_query($query);
 		$query = "select * from ms_board_table where isdel != '1' order by idx desc limit 3,6";
 		$result2 = $this->old_model->run_query($query);
-		$this->document->view('main/old_main', array(
+		$this->load->view('main/old_main', array(
         			'result' 	=> $result,
         			'result2'	=> $result2
         ));
