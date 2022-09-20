d0f21d23662b22835eeb3d2e79e8cdae27081831
shawwwnie <sanghoon1549@gmail.com>
2022-09-20 23:03:33 +0900

    edit old main

--- application/views/main/old_main.php
+++ application/views/main/old_main.php
@@ -156,12 +156,12 @@ include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/old/head_ca.php');
     
 	<div class="swiper c3Swiper">
       <div class="swiper-wrapper">
-        <a href="/static/img/old/after_ka_1.jpg" target="_blank"><div class="swiper-slide"><img src="/static/img/old/after_ka_1.jpg"></div></a>
-        <a href="/static/img/old/after_ka_2.jpg" target="_blank"><div class="swiper-slide"><img src="/static/img/old/after_ka_2.jpg"></div></a>
-        <a href="/static/img/old/after_ka_3.jpg" target="_blank"><div class="swiper-slide"><img src="/static/img/old/after_ka_3.jpg"></div></a>
-        <a href="/static/img/old/after_ka_4.jpg" target="_blank"><div class="swiper-slide"><img src="/static/img/old/after_ka_4.jpg"></div></a>
-        <a href="/static/img/old/after_ka_5.jpg" target="_blank"><div class="swiper-slide"><img src="/static/img/old/after_ka_5.jpg"></div></a>
-		<a href="/static/img/old/after_ka_7.jpg" target="_blank"><div class="swiper-slide"><img src="/static/img/old/after_ka_7.jpg"></div></a>
+        <div class="swiper-slide"><a href="/static/img/old/after_ka_1.jpg" target="_blank"><img src="/static/img/old/after_ka_1.jpg"></a></div>
+        <div class="swiper-slide"><a href="/static/img/old/after_ka_2.jpg" target="_blank"><img src="/static/img/old/after_ka_2.jpg"></a></div>
+        <div class="swiper-slide"><a href="/static/img/old/after_ka_3.jpg" target="_blank"><img src="/static/img/old/after_ka_3.jpg"></a></div>
+        <div class="swiper-slide"><a href="/static/img/old/after_ka_4.jpg" target="_blank"><img src="/static/img/old/after_ka_4.jpg"></a></div>
+        <div class="swiper-slide"><a href="/static/img/old/after_ka_5.jpg" target="_blank"><img src="/static/img/old/after_ka_5.jpg"></a></div>
+		<div class="swiper-slide"><a href="/static/img/old/after_ka_7.jpg" target="_blank"><img src="/static/img/old/after_ka_7.jpg"></a></div>
       </div>
       <div class="swiper-button-next"></div>
       <div class="swiper-button-prev"></div>
