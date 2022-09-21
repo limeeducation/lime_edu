75f0b7be160d9cd276dfe38d794b90bb2f973b26
shawwwnie <sanghoon1549@gmail.com>
2022-09-22 00:38:39 +0900

    apply old pages

--- application/views/old/board/board_view.php
+++ application/views/old/board/board_view.php
@@ -12,7 +12,7 @@ include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/old/head_ca.php');
 </div>
 
 
-<?php print_r($contents[0].$title);?>
+<?php print_r($contents[0]->title);?>
 	<div class="up-on-scroll txbox">
 		<h4 class="sub_tx_tit"><?=$contents[0].$title?></h4>
 		<div class="tblbox le">
