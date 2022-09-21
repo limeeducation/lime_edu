1d398e959f7fefbb55c987ce4e7eaa18ba2624ed
shawwwnie <sanghoon1549@gmail.com>
2022-09-22 00:58:04 +0900

    apply old pages

--- application/views/old/board/board_view.php
+++ application/views/old/board/board_view.php
@@ -5,7 +5,7 @@ include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/old/head_ca.php');
 
 <div id="container">
 
-<div class="sub_tit">
+<div class="sub_tit" style="background-image: url(/static/img/old/data/board/<?=$img_file[0]?>); background-size: cover; background-position: center;">
 	<div class="stt_box">
 		<h3 class="tit_tx">유학컬럼</h3>
 	</div>
@@ -14,7 +14,6 @@ include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/old/head_ca.php');
 $img_file = explode("|",$contents[0]->filename_real1);
 ?>
 <div class="sb_con_all">
-	<div class="up-on-scroll txbox"><img src="/static/img/old/data/board/<?=$img_file[0]?>" alt=" 사진"></div>
 	<div class="up-on-scroll txbox">
 		<h4 class="sub_tx_tit"><?=$contents[0]->title?></h4>
 		<div class="tblbox le">
