086c3cb5c4c9f762523c7b79db0641938832b687
shawwwnie <sanghoon1549@gmail.com>
2022-09-22 00:49:23 +0900

    apply old pages

--- application/views/old/board/board_view.php
+++ application/views/old/board/board_view.php
@@ -13,14 +13,15 @@ include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/old/head_ca.php');
 <?php
 $img_file = explode("|",$contents[0]->filename_real1);
 ?>
-<div class="up-on-scroll txbox"><img src="/static/img/old/data/board/<?=$img_file[0]?>" alt=" 사진"></div>
+<div class="sb_con_all">
+	<div class="up-on-scroll txbox"><img src="/static/img/old/data/board/<?=$img_file[0]?>" alt=" 사진"></div>
 	<div class="up-on-scroll txbox">
 		<h4 class="sub_tx_tit"><?=$contents[0]->title?></h4>
 		<div class="tblbox le">
 			<?=$contents[0]->contents?>
 		</div>
 	</div>
-
+</div>
 <?php
 include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/footer_temp.php');
 ?>
