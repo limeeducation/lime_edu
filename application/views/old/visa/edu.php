7f6be24d65be27887554cd942000e6769af89a9b
shawwwnie <sanghoon1549@gmail.com>
2022-09-27 22:46:06 +0900

    apply old pages

--- application/views/old/visa/edu.php
+++ application/views/old/visa/edu.php
@@ -172,7 +172,10 @@ include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/old/head_ca.php');
 </div>
 <script>
 	window.onresize = function(event){
-      window.resizeTo(window.outerWidth, Math.floor(window.outerWidth/4*3));
+	  window.resizeTo(window.outerWidth, Math.floor(window.outerWidth/4*3));
+      if((window.outerWidth == screen.width) || (window.outerHeight == screen.height)) {
+		alert("최대화면!");
+	  }
     }
 </script>
 <?php
