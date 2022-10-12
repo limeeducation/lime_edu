e36da2cb3adb529dfbb9703bc48846fce086739d
shawwwnie <sanghoon1549@gmail.com>
2022-10-13 01:50:50 +0900

    add camp pages

--- application/views/old/camp/view.php
+++ application/views/old/camp/view.php
@@ -24,10 +24,9 @@ include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/old/head_ca.php');
 <body>
 	<div class="top_back"></div>
 	<div class="content_outer">
-
-		<img src="/static/img/old/camp/am_east_pri_camp.png" class="inner_content">
-		<a href="#none" class="btn_consult"></a>
-		</img>
+		<a href="#none" class="btn_consult">
+			<img src="/static/img/old/camp/am_east_pri_camp.png" class="inner_content">
+		</a>
 		<img src="/static/img/old/camp/ca_pub_sch.png" class="inner_content">
 		<img src="/static/img/old/camp/phi_tallac_fa.png" class="inner_content">
 		<img src="/static/img/old/camp/phil_ceb_camp.png" class="inner_content">
