9ddc6487e1fc62f944162a54f9353fc08467978e
shawwwnie <sanghoon1549@gmail.com>
2022-10-13 01:36:07 +0900

    add camp pages

--- application/views/old/camp/view.php
+++ application/views/old/camp/view.php
@@ -3,12 +3,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/old/head_ca.php');
 ?>
 <link href="/static/css/old/main.css" rel="stylesheet" >
-<img src="/static/img/old/camp/am_east_pri_camp.png">
-<img src="/static/img/old/camp/ca_pub_sch.png">
-<img src="/static/img/old/camp/phi_tallac_fa.png">
-<img src="/static/img/old/camp/phil_ceb_camp.png">
-<img src="/static/img/old/camp/phil_tallac_camp.png">
-<img src="/static/img/old/camp/uk_pub_sch.png">
+<style>
+	.top_back{
+		height:120px;
+		background-color:#00000057;
+	}
+	.content_outer{
+		width:80%;
+	}
+	.inner_content{
+		width:50%;
+	}
+</style>
+<body>
+	<div class="top_back">
+	</div>
+	<div class="content_outer">
+		<img src="/static/img/old/camp/am_east_pri_camp.png" class="inner_content">
+		<img src="/static/img/old/camp/ca_pub_sch.png" class="inner_content">
+		<img src="/static/img/old/camp/phi_tallac_fa.png" class="inner_content">
+		<img src="/static/img/old/camp/phil_ceb_camp.png" class="inner_content">
+		<img src="/static/img/old/camp/phil_tallac_camp.png" class="inner_content">
+		<img src="/static/img/old/camp/uk_pub_sch.png" class="inner_content">
+	</div>
+</body>
 <?php
 include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/footer_temp.php');
 ?>
