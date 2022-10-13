f9cb63e2aa552680b740b948367256fc8c40637d
shawwwnie <sanghoon1549@gmail.com>
2022-10-13 23:54:07 +0900

    add camp pages

--- application/views/old/camp/view.php
+++ application/views/old/camp/view.php
@@ -18,7 +18,7 @@ include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/old/head_ca.php');
 		margin:1%;
 	}
 	.content_outer .img_wrap img {display:block; width:100%;}
-	.content_outer .img_wrap {position:relative;}
+	.content_outer .img_wrap {position:relative; display:inline-block;}
 	.content_outer .img_wrap .btn_consult {position:absolute; left:50%; transform:translate(-50%); bottom:0; width:100%; height:11%;}
 
 	@media(max-width: 768px){
