0a4ee7612bdb880bf6ba03ff3af1f825c60b1c61
shawwwnie <sanghoon1549@gmail.com>
2022-11-12 11:21:27 +0900

    Admin main page customize

--- static/js/admin/main.js
+++ static/js/admin/main.js
@@ -65,7 +65,7 @@
 
 
     // Worldwide Sales Chart
-    var ctx1 = $("#worldwide-sales").get(0).getContext("2d");
+    /*var ctx1 = $("#worldwide-sales").get(0).getContext("2d");
     var myChart1 = new Chart(ctx1, {
         type: "bar",
         data: {
@@ -90,11 +90,11 @@
         options: {
             responsive: true
         }
-    });
+    });*/
 
 
     // Salse & Revenue Chart
-    var ctx2 = $("#salse-revenue").get(0).getContext("2d");
+    /*var ctx2 = $("#salse-revenue").get(0).getContext("2d");
     var myChart2 = new Chart(ctx2, {
         type: "line",
         data: {
@@ -116,12 +116,12 @@
         options: {
             responsive: true
         }
-    });
+    });*/
     
 
 
     // Single Line Chart
-    var ctx3 = $("#line-chart").get(0).getContext("2d");
+    /*var ctx3 = $("#line-chart").get(0).getContext("2d");
     var myChart3 = new Chart(ctx3, {
         type: "line",
         data: {
@@ -136,11 +136,11 @@
         options: {
             responsive: true
         }
-    });
+    });*/
 
 
     // Single Bar Chart
-    var ctx4 = $("#bar-chart").get(0).getContext("2d");
+    /*var ctx4 = $("#bar-chart").get(0).getContext("2d");
     var myChart4 = new Chart(ctx4, {
         type: "bar",
         data: {
@@ -159,12 +159,12 @@
         options: {
             responsive: true
         }
-    });
+    });*/
 
 
     // Pie Chart
     var ctx5 = $("#pie-chart").get(0).getContext("2d");
-    var myChart5 = new Chart(ctx5, {
+    /*var myChart5 = new Chart(ctx5, {
         type: "pie",
         data: {
             labels: ["Italy", "France", "Spain", "USA", "Argentina"],
@@ -182,11 +182,11 @@
         options: {
             responsive: true
         }
-    });
+    });*/
 
 
     // Doughnut Chart
-    var ctx6 = $("#doughnut-chart").get(0).getContext("2d");
+    /*var ctx6 = $("#doughnut-chart").get(0).getContext("2d");
     var myChart6 = new Chart(ctx6, {
         type: "doughnut",
         data: {
@@ -205,7 +205,7 @@
         options: {
             responsive: true
         }
-    });
+    });*/
 
     
 })(jQuery);
