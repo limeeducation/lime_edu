3f48a1c786f853d54fdf2996635dd61a6ec3972b
shawwwnie <sanghoon1549@gmail.com>
2022-11-12 11:48:23 +0900

    Admin main page customize

--- application/views/layout/admin/sidebar.php
+++ application/views/layout/admin/sidebar.php
@@ -7,7 +7,7 @@
 	?>
 	<nav class="navbar bg-secondary navbar-dark">
 		<a href="/admin/main" class="navbar-brand mx-4 mb-3">
-			<h3 class="text-success"><i class="fa fa-user-edit me-2"></i>LIME EDU</h3>
+			<h3 class="text-success"><i class="fa fa-user-edit me-2"></i>관리자페이지</h3>
 		</a>
 		<div class="d-flex align-items-center ms-4 mb-4">
 			<div class="ms-3">
@@ -16,7 +16,7 @@
 			</div>
 		</div>
 		<div class="navbar-nav w-100">
-			<a href="/admin/main" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
+			<a href="/admin/main" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>대시보드</a>
 			<div class="nav-item dropdown">
 				<a href="" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fas fa-images me-2"></i>배너관리</a>
 				<div class="dropdown-menu bg-transparent border-0">
@@ -46,6 +46,12 @@
 				<div class="dropdown-menu bg-transparent border-0">
 					<a href="button.html" class="dropdown-item">상담학생 관리</a>
 					<a href="typography.html" class="dropdown-item">수속학생 관리</a>
+				</div>
+			</div>
+			<div class="nav-item dropdown">
+				<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fas fa-edit me-2"></i>컨텐츠관리</a>
+				<div class="dropdown-menu bg-transparent border-0">
+					<a href="button.html" class="dropdown-item">칼럼 관리</a>
 				</div>
 			</div>
 		</div>
