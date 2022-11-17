566fa777cb8243a629979639815cc44a9a8c0998
shawwwnie <sanghoon1549@gmail.com>
2022-11-18 00:28:13 +0900

    banner list add

--- application/views/admin/bannerList.php
+++ application/views/admin/bannerList.php
@@ -21,14 +21,34 @@
 				include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/admin/header-gnb.php');
 			?>
 			<div class="container-fluid pt-4 px-4">
-                <div class="bg-secondary text-center rounded p-4">
-                    <div class="d-flex align-items-center justify-content-between mb-4">
+                <div class="bg-secondary text-center rounded p-2">
+                    <div class="d-flex align-items-center justify-content-between ">
 						<h6 class="mb-0"><button type="button" class="btn btn-lg btn-outline-success m-2">TAB 1</button></h6>
 						<h6 class="mb-0"><button type="button" class="btn btn-lg btn-outline-success m-2">TAB 2</button></h6>
 						<h6 class="mb-0"><button type="button" class="btn btn-lg btn-outline-success m-2">TAB 3</button></h6>
-						<a href="" ><?= $type; ?></a>
+						<a href="" >순서 변경하기</a>
 					</div>
                 </div>
+                <div class="bg-secondary rounded p-2 mt-3">
+					<form style="display: flex;">
+						<div class="mb-3 col-xl-5" style="height: 270px;">
+							<img src="https://drive.google.com/file/d/155w1uIZUWoJobQppx--W1NJ8odKJlIyK/view?usp=sharing">
+							<div class="form-text">
+								이미지 권장 사이즈 : @@@@ X @@@@
+							</div>
+						</div>
+						<div class="mb-3 col-xl-6 mx-3">
+							<label for="exampleInputPassword1" class="form-label">이미지 URL</label>
+							<input type="password" class="form-control" id="exampleInputPassword1">
+							<label for="exampleInputPassword1" class="form-label">연결 URL</label>
+							<input type="password" class="form-control" id="exampleInputPassword1">
+							<label for="exampleInputPassword1" class="form-label">노출 시작일</label>
+							<input type="password" class="form-control" id="exampleInputPassword1">
+							<label for="exampleInputPassword1" class="form-label">노출 종료일</label>
+							<input type="password" class="form-control" id="exampleInputPassword1">
+						</div>
+					</form>
+				</div>
             </div>
 
             <?php
