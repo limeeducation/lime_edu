4d751f9d7999c2269f2e24ef8d7a8c3f2a52ad4e
shawwwnie <sanghoon1549@gmail.com>
2022-11-17 23:44:31 +0900

    banner list add

--- application/models/Admin_prod_model.php
+++ application/models/Admin_prod_model.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Admin_model extends CI_Model
+class Admin_prod_model extends CI_Model
 {
 	public function __construct()
 	{
