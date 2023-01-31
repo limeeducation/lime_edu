ec8f241f6580ae17f626a75b16079b7e681c0071
shawwwnie <sanghoon1549@gmail.com>
2023-02-01 01:18:29 +0900

    우측 내비바 수정

--- application/views/main/main.php
+++ application/views/main/main.php
@@ -161,25 +161,6 @@ include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/head.php');
 					</div><!-- // inner -->
 				</section>
 				<!-- // mainBanner -->
-<div class="quick_nav">
-
-	<div class="quick_title">유학 관련 궁금한 점은 부담없이 문의하세요!</div>
-	<ul class="quick_list">
-		<li class="phone"><a href="#">전화 상담 예약</a></li>
-		<li class="home"><a href="#">방문 상담 예약</a></li>
-		<li class="kakao"><a href="#">실시간 상담</a></li>
-	</ul>
-	<ul class="quick_time">
-		<li>평일 00:00AM~00:00PM</li>
-		<li>주말 00:00AM~00:00PM</li>
-	</ul>
-	<ul class="quick_contact">
-		<li class="email">hm.lee@mylimeedu.com</li>
-		<li class="tel">02-2135-7699</li>
-	</ul>
-
-</div>
-
 			</div>
 			<!-- // content -->
 		</div>
