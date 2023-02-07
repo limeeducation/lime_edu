75f3b582f96e04d714c660cc7b700477ced0d135
shawwwnie <sanghoon1549@gmail.com>
2023-02-08 04:03:35 +0900

    메인 수정

--- application/views/main/main.php
+++ application/views/main/main.php
@@ -17,7 +17,7 @@ include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/head.php');
 				$("#banr_list_"+nation).show();
             }
         }
-        $("#eng_city_btn").find('img').hover(function(){
+        $(".eng_city_btn").find('img').hover(function(){
         	$(this).attr("src", $(this).attr("src").replace(".png", "_after.png"));
         }, function(){
         	$(this).attr("src", $(this).attr("src").replace("_after.png", ".png"));
