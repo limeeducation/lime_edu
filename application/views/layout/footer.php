cc7e64ebda0d256e12d2eb1b7f7137d79440f76f
shawwwnie <sanghoon1549@gmail.com>
2023-02-07 16:48:40 +0900

    팝업 삭제

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -37,10 +37,7 @@
 					</dl>
 				</div>
 				<div class="footer_copy">© 2022 Copyright: mylimeedu.com</div>
-			</div><!-- // inner
-			<!-- <link rel="stylesheet" type="text/css" href="/static/css/consultPopup/sangdam-yeyag-pabeob.css" />
-            <link rel="stylesheet" type="text/css" href="/static/css/consultPopup/styleguide.css" />
-            <link rel="stylesheet" type="text/css" href="/static/css/consultPopup/globals.css" /> -->
+			</div><!-- // inner -->
 		</footer>
 
 
--- application/views/layout/quick_nav.php
+++ application/views/layout/quick_nav.php
@@ -3,7 +3,7 @@
 <div class="quick_nav">
 	<div class="quick_title">유학 관련 궁금한 점은 부담없이 문의하세요!</div>
 	<ul class="quick_list">
-		<li class="cal"><a href="javascript:showConsult();">상담 예약</a></li>
+		<li class="cal"><a href="javascript:on_construct();">상담 예약</a></li>
 		<div id="kakao-talk-channel-chat-button" data-channel-public-id="_mnYdxj" data-title="consult" data-size="large" data-color="yellow" data-shape="mobile" data-support-multiple-densities="true" style="margin-top:1.2rem;"></div>
 	</ul>
 	<ul class="quick_time">
@@ -19,9 +19,7 @@
 		<li class="tel">02-2135-7699</li>
 	</ul>
 </div>
-	<?php
-		include($_SERVER['DOCUMENT_ROOT'].'/application/views/layout/consult_popup.php');
-	?>
+
 <script>
   window.kakaoAsyncInit = function() {
     Kakao.Channel.createChatButton({container: '#kakao-talk-channel-chat-button', });
@@ -37,8 +35,5 @@
     fjs.parentNode.insertBefore(js, fjs);
   })(document, 'script', 'kakao-js-sdk');
 
-  function showConsult(){
-  	$(".sangdam-yeyag-pabeob").show();
-  }
 </script>
 <!-- // quick_nav -->
