e7667cf513b9fe7ddf708d3460d87698b0eef2ef
shawwwnie <sanghoon1549@gmail.com>
2023-02-13 20:55:21 +0900

    헤드 수정

--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -11,7 +11,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta name="format-detection" content="telephone=no" />
 	<meta property="og:title" content="라임에듀">
 	<meta property="og:description" content="라임에듀">
+	<meta property="og:type" content="website">
 	<meta name="description" content="라임에듀">
+	<meta property="og:image" content="https://mylimeedu.com/static/img/common/header_logo_2@2x.png">
 
 	<link rel="stylesheet" type="text/css" href="https://spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css">
 	<link rel="stylesheet" type="text/css" href="/static/css/reset.css">
