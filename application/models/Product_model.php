8a4c00663f460b52217b6e0d7c914e210aacf830
shawwwnie <sanghoon1549@gmail.com>
2023-02-17 01:12:34 +0900

    상품 상세 페이지

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Banner_model extends CI_Model
+class Product_model extends CI_Model
 {
 	public function __construct()
 	{
