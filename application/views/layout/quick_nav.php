d394eaf3387b257ecff52ac758fcca3ef2e52026
applicant007 <sanghoon1549@gmail.com>
2023-11-15 08:32:31 +0900

    카카오 채널 삽입

--- application/views/layout/quick_nav.php
+++ application/views/layout/quick_nav.php
@@ -4,7 +4,7 @@
 	<div class="quick_title">유학 관련 궁금한 점은 부담없이 문의하세요!</div>
 	<ul class="quick_list">
 		<li class="cal"><a href="javascript:;" class="popup_open"></a></li>
-		<div id="kakao-talk-channel-chat-button"  class="kakao_chat" data-channel-public-id="_mnYdxj" data-title="consult" data-size="large" data-color="yellow" data-shape="mobile" data-support-multiple-densities="true" style="margin-top:1.2rem;"></div>
+		<div id="kakao-add-channel-button"  class="kakao_chat" data-channel-public-id="_mnYdxj" data-title="consult" data-size="large" data-color="yellow" data-shape="mobile" data-support-multiple-densities="true" style="margin-top:1.2rem;"></div>
 	</ul>
 	<ul class="quick_time">
 		<li>평일 10:00AM~19:00PM</li>
@@ -22,7 +22,7 @@
 
 <script>
   window.kakaoAsyncInit = function() {
-    Kakao.Channel.createChatButton({container: '#kakao-talk-channel-chat-button', });
+    Kakao.Channel.createAddChannelButton({container: '#kakao-add-channel-button', channelPublicId: '_mnYdxj'});
   };
 
   (function(d, s, id) {
