93a77af4ba93a241a23a5fd484e57d6cbccee95c
applicant007 <sanghoon1549@gmail.com>
2023-12-05 00:02:45 +0900

    푸터 주소 수정

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -6,7 +6,7 @@
 						<dt>서울 강남센터</dt>
 						<dd>
 							<p class="tel">02-2135-7699</p>
-							<p class="addr">서울시 강남구 강남대로 310 유니온센터 1204호</p>
+							<p class="addr">서울시 서초구 사임당로 174 강남미래타워 602호</p>
 						</dd>
 					</dl>
 					<dl class="footer_data">
@@ -17,10 +17,12 @@
 						<dd>
 							<p class="addr">인천 송도 센터</p>
 							<p>인천광역시 연수구 컨벤시아대로 165 포스코타워 26층</p>
-							<p class="addr">대구 동성로 센터</p>
-							<p>대구광역시 중구 중앙대로 385-1 3층</p>
-							<p class="addr">부산 서면 센터</p>
-							<p>부산시 진구 동천로 109 삼한골든게이트 빌딩 9층</p>
+							<p class="addr">대전센터</p>
+							<p>대전시 서구 청사로 136 대전무역회관 18층</p>
+							<p class="addr">대구센터</p>
+							<p>대구시 중구 중앙대로 385-1 3층</p>
+							<p class="addr">부산 서면센터</p>
+                            <p>부산시 진구 동천로 109 삼한골든게이트 빌딩 9층</p>
 							<p class="addr">전남 센터</p>
                             <p>전라남도 순천시 삼산로 15-1, 3층</p>
 						</dd>
@@ -28,13 +30,30 @@
 					<dl class="footer_data">
 						<dt>해외 직영센터</dt>
 						<dd>
-							<p class="tel">대표번호 +1 647-427-2006</p>
+							<p class="tel">대표번호 +1-647-427-2006</p>
 						</dd>
 						<dd>
 							<p class="addr">캐나다 토론토 센터</p>
 							<p>55 Eglinton Ave East, Suite 601, Toronto ON M4P 1G8 Canada</p>
-							<p class="addr">캐나다 벤쿠버 센터</p>
-							<p>Suite 1100 - 789 W Pender St Vancouver, BC V6C 1H2</p>
+							<p class="addr">캐나다 옥빌센터 - 조기유학/동부 탑보딩 관리 업무(애플비, 리들리 등)</p>
+							<p>1054 John Laird Circle, Oakville ON L6H 3R5 Canada</p>
+						</dd>
+					</dl>
+					<dl class="footer_data">
+						<dt>해외 협력지사</dt>
+						<dd>
+							<p>영국 런던센터</p>
+							<p class="addr">1 Partridge Mews, Chertsey, Surrey, KT16 9GB</p>
+							<p class="tel">+44-7796-575950</p>
+						</dd>
+						<dd>
+							<p>캐나다 밴쿠버센터</p>
+							<p class="addr">789 W Pender St, Suite 1100, Vancouver, BC V6C 1H2</p>
+						</dd>
+						<dd>
+							<p>뉴질랜드 오클랜드센터</p>
+							<p class="addr">18 Cayman place. Usworth Heights. North Shore, Auckland New Zealand </p>
+							<p class="tel">+6421308802</p>
 						</dd>
 					</dl>
 				</div>
